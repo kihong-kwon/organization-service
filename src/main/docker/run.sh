39caa78a3ea5ff046a537575f1ec0c73ae40c0dd
kwon-kihong <kihong.kwon7389@gmail.com>
2019-08-22 22:54:44 +0900

    test

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -3,20 +3,19 @@
 echo "********************************************************"
 echo "Waiting for the eureka server to start on port $EUREKASERVER_PORT"
 echo "********************************************************"
-while ! `nc -z eurekaserver  $EUREKASERVER_PORT`; do sleep 3; done
+while ! `nc -z spring-cloud-eurekasvr-svc $EUREKASERVER_PORT`; do sleep 3; done
 echo "******* Eureka Server has started"
-
 
 echo "********************************************************"
 echo "Waiting for the database server to start on port $DATABASESERVER_PORT"
 echo "********************************************************"
-while ! `nc -z postgresql $DATABASESERVER_PORT`; do sleep 3; done
+while ! `nc -z spring-msa-db-svc $DATABASESERVER_PORT`; do sleep 3; done
 echo "******** Database Server has started "
 
 echo "********************************************************"
 echo "Waiting for the configuration server to start on port $CONFIGSERVER_PORT"
 echo "********************************************************"
-while ! `nc -z configserver $CONFIGSERVER_PORT`; do sleep 3; done
+while ! `nc -z spring-cloud-config-svc $CONFIGSERVER_PORT`; do sleep 3; done
 echo "*******  Configuration Server has started"
 
 echo "********************************************************"
